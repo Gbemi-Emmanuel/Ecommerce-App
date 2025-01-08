37d38582a9f522391dd477f8a7aa77db3f95f626
unknown <gbemiemmanuel555@gmail.com>
2025-01-08 11:31:22 -0800

    Home page frontend completed

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -10,6 +10,6 @@ createRoot(document.getElementById('root')).render(
     <ShopContextProvider>
       <App />
     </ShopContextProvider>
-  </BrowserRouter>,
+  </BrowserRouter>
 )
  
--- Frontend/src/pages/Collection.jsx
+++ Frontend/src/pages/Collection.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { ShopContext } from '../context/ShopContext'
+import { assets } from '../assets/assets';
 
 const Collection = () => {
+
+  const { products } = useContext(ShopContext);
+  const [showFilter, setShowFiler] = useState(false)
+
   return (
-    <div>
+    <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
       
+      {/* FILTER OPTION */}
+      <div className="min-w-60">
+        <p className='my-2 text-xl flex items-center cursor-pointer gap-2'> 
+          FLITERS
+          <img className={`pl-1 h-3 sm:hidden ${showFilter ? 'rotate-90' : ''}`} src={assets.dropdown_icon} alt="" />
+        </p>
+        {/* CATEGORY FLITER */}
+        <div className={`border border-gray-300 pl-5 py-3 mt-6 ${showFilter ? '' : 'hidden'} sm:block`}>
+          <p className='mb-3 text-sm font-medium'>CATEGORIES</p>
+          <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
+            <p className='flex gap-2'>
+              <input className='w-3' type="checkbox" value={'Men'} /> Men
+            </p>
+            <p className='flex gap-2'>
+              <input className='w-3' type="checkbox" value={'Women'} /> Women
+            </p>
+            <p className='flex gap-2'>
+              <input className='w-3' type="checkbox" value={'kids'} /> kids
+            </p>
+          </div>
+        </div>
+        {/* SUBCATEGORY FILTER */}
+        <div className={`border border-gray-300 pl-5 py-3 my-5 ${showFilter ? '' : 'hidden'} sm:block`}>
+          <p className='mb-3 text-sm font-medium'>TYPE</p>
+          <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
+            <p className='flex gap-2'>
+              <input className='w-3' type="checkbox" value={'Topwear'} /> Top wears
+            </p>
+            <p className='flex gap-2'>
+              <input className='w-3' type="checkbox" value={'Bottomwaer'} /> Botton wears
+            </p>
+            <p className='flex gap-2'>
+              <input className='w-3' type="checkbox" value={'Winterwear'} /> Winter wears
+            </p>
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
