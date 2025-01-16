984064dcb1dedfeada3eaf0c7415906267041d74
unknown <gbemiemmanuel555@gmail.com>
2025-01-16 14:15:34 -0800

    Cart page completed

--- Frontend/src/context/ShopContext.jsx
+++ Frontend/src/context/ShopContext.jsx
@@ -1,6 +1,7 @@
-import { createContext, useEffect, useState, } from "react";
+import { createContext, useEffect, useState,} from "react";
 import { products } from "../assets/assets";
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom"
 
 export const ShopContext = createContext();
 
